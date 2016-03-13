16ff3786046de65b0f4e623f775e0a4cce0fc404
Zanexess <sesldark@yandex.ru>
2016-03-13 09:29:17 +0300

    Added ActionBar to SecondActivity. Small fix

--- app/src/main/java/com/zanexes/technotrack_android_01/FirstActivity.java
+++ app/src/main/java/com/zanexes/technotrack_android_01/FirstActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 public class FirstActivity extends Activity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/zanexes/technotrack_android_01/SecondActivity.java
+++ app/src/main/java/com/zanexes/technotrack_android_01/SecondActivity.java
@@ -1,16 +1,15 @@
 package com.zanexes.technotrack_android_01;
 
-import android.app.Activity;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ListView;
 
 import com.zanexes.technotrack_android_01.adapter.ListViewAdapter;
 
-public class SecondActivity extends Activity {
-    private AsyncTask asyncTask;
-
+public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +20,12 @@ public class SecondActivity extends Activity {
     private void initListView(Context context) {
         ListView lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(new ListViewAdapter(context));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
